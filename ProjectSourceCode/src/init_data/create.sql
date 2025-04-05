84b832df1c9cf54213055fa56a7104c7487dbc80
JRhooooo <128426001+JRhooooo@users.noreply.github.com>
2025-04-05 15:10:13 -0600

    Update create.sql

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,4 +1,6 @@
-CREATE TABLE users (
-    username VARCHAR(50) PRIMARY KEY,
-    password VARCHAR(60) NOT NULL
+CREATE TABLE USERS (
+  student_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(200) NOT NULL,
 );
